48de25d8a91aefee8af58a297162851bbf6c1660
Joost van Uitert <joost.van.uitert@minbuza.onmicrosoft.com>
2021-09-26 11:42:22 +0200

    Working on making legal moves

---
+++ Models/Pawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace UltraChess.Blazor.Models
+{
+    public class Pawn : Piece
+    {
+        public List<int> GetSquaresToMoveTo(int fromSquare)
+        {
+            List<int> squaresToMoveTo = new();
+            if (IsWhite)
+            {
+                squaresToMoveTo.Add(fromSquare - 8);
+                if(fromSquare > 47 && fromSquare < 56)
+                {
+                    squaresToMoveTo.Add(fromSquare - 16);
+                }
+            }
+            else
+            {
+                squaresToMoveTo.Add(fromSquare + 8 );
+                if (fromSquare > 7 && fromSquare < 16)
+                {
+                    squaresToMoveTo.Add(fromSquare + 16);
+                }
+            }            
+
+            return squaresToMoveTo;
+        }
+    }
+}
---
+++ Models/Square.cs
@@ -0,0 +1,12 @@
+﻿namespace UltraChess.Blazor.Models
+{
+    public class Square
+    {
+        public int Id { get; set; }
+        public char File { get; set; }
+        public char Rank { get; set; }
+        public bool IsLight { get; set; }
+        public bool IsHighlighted { get; set; }
+        public Piece Piece { get; set; }
+    }
+}
