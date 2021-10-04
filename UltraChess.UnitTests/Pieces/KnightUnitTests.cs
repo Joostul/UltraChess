8d8956d8a51e82b99d31973264c538d655fad763
Joost van Uitert <joost.van.uitert@minbuza.onmicrosoft.com>
2021-10-05 00:10:23 +0200

    Fix knight unit tests

--- UltraChess.UnitTests/Pieces/KnightUnitTests.cs
+++ UltraChess.UnitTests/Pieces/KnightUnitTests.cs
@@ -1,40 +1,52 @@
-﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Shouldly;
-//using System.Collections.Generic;
-//using UltraChess.Blazor.Models;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System.Collections.Generic;
+using UltraChess.Blazor.Models;
 
-//namespace UltraChess.UnitTests.Pieces
-//{
-//    [TestClass]
-//    public class KnightUnitTests
-//    {
-//        [TestMethod]
-//        public void GetKnightMoves_DefaultBoard_SquareIndex1()
-//        {
-//            // Arrange
-//            var sut = new ChessBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-//            {
-//                IsWhiteTurn = false
-//            };
+namespace UltraChess.UnitTests.Pieces
+{
+    [TestClass]
+    public class KnightUnitTests
+    {
+        [TestMethod]
+        public void GetKnightMoves_DefaultBoard_SquareIndex1()
+        {
+            // Arrange
+            var sut = new ChessBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+            {
+                IsWhiteTurn = false
+            };
 
-//            // Act
-//            var moves = sut.GetMovementSquares(1, sut.IsWhiteTurn);
+            // Act
+            var moves = sut.GetMovesFromSquare(1, sut.IsWhiteTurn);
 
-//            // Assert
-//            moves.ShouldBe(new List<int> { 16, 18 });
-//        }
+            // Assert
+            var expectedMoves = new List<Move> { new Move(1, 16), new Move(1, 18) };
+            foreach (var move in expectedMoves)
+            {
+                moves.ShouldContain(m => m.ToSquareId == move.ToSquareId && m.FromSquareId == move.FromSquareId && m.CapturedPieceId == move.CapturedPieceId,
+                    $"Move from: {move.FromSquareId} to: {move.ToSquareId}, capturing: {move.CapturedPieceId} not found.");
+            }
+            moves.Count.ShouldBe(2);
+        }
 
-//        [TestMethod]
-//        public void GetKnightMoves_DefaultBoard_SquareIndex57()
-//        {
-//            // Arrange
-//            var sut = new ChessBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        [TestMethod]
+        public void GetKnightMoves_DefaultBoard_SquareIndex57()
+        {
+            // Arrange
+            var sut = new ChessBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
-//            // Act
-//            var moves = sut.GetMovementSquares(57, sut.IsWhiteTurn);
+            // Act
+            var moves = sut.GetMovesFromSquare(57, sut.IsWhiteTurn);
 
-//            // Assert
-//            moves.ShouldBe(new List<int> { 40, 42 });
-//        }
-//    }
-//}
+            // Assert
+            var expectedMoves = new List<Move> { new Move(57, 40), new Move(57, 42) };
+            foreach (var move in expectedMoves)
+            {
+                moves.ShouldContain(m => m.ToSquareId == move.ToSquareId && m.FromSquareId == move.FromSquareId && m.CapturedPieceId == move.CapturedPieceId,
+                    $"Move from: {move.FromSquareId} to: {move.ToSquareId}, capturing: {move.CapturedPieceId} not found.");
+            }
+            moves.Count.ShouldBe(2);
+        }
+    }
+}
