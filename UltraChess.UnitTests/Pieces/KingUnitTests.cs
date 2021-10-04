0efb7b29d442e0ac263956a9441c63570133d248
Joost van Uitert <joost.van.uitert@minbuza.onmicrosoft.com>
2021-10-05 00:08:57 +0200

    Fix king unit tests

--- UltraChess.UnitTests/Pieces/KingUnitTests.cs
+++ UltraChess.UnitTests/Pieces/KingUnitTests.cs
@@ -1,29 +1,33 @@
-﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Shouldly;
-//using System.Collections.Generic;
-//using System.Linq;
-//using UltraChess.Blazor.Models;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System.Collections.Generic;
+using UltraChess.Blazor.Models;
 
-//namespace UltraChess.UnitTests.Pieces
-//{
-//    [TestClass]
-//    public class KingUnitTests
-//    {
-//        [TestMethod]
-//        public void GetKingMovesTest()
-//        {
-//            // Arrange
-//            var sut = new ChessBoard("k7/8/8/8/8/8/8/8 w KQkq - 0 1")
-//            {
-//                IsWhiteTurn = false
-//            };
+namespace UltraChess.UnitTests.Pieces
+{
+    [TestClass]
+    public class KingUnitTests
+    {
+        [TestMethod]
+        public void GetKingMovesTest()
+        {
+            // Arrange
+            var sut = new ChessBoard("k6K/8/8/8/8/8/8/8 w KQkq - 0 1")
+            {
+                IsWhiteTurn = false
+            };
 
-//            // Act
-//            var moves = sut.GetMovementSquares(0, sut.IsWhiteTurn);
-//            //var moves = new List<int> { 9, 18, 27, 36, 45, 54, 63 };
+            // Act
+            var moves = sut.GetMovesFromSquare(0, sut.IsWhiteTurn);
 
-//            // Assert
-//            moves.OrderBy(m => m).ShouldBe(new List<int> { 1, 8, 9 });
-//        }
-//    }
-//}
+            // Assert
+            var expectedMoves = new List<Move> { new Move(0, 1), new Move(0, 8), new Move(0, 9)};
+            foreach (var move in expectedMoves)
+            {
+                moves.ShouldContain(m => m.ToSquareId == move.ToSquareId && m.FromSquareId == move.FromSquareId && m.CapturedPieceId == move.CapturedPieceId,
+                    $"Move from: {move.FromSquareId} to: {move.ToSquareId}, capturing: {move.CapturedPieceId} not found.");
+            }
+            moves.Count.ShouldBe(3);
+        }
+    }
+}
